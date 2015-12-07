bc01e4d623f15b54c8e06a176a15b64583b57fe3
SamEureka <sameureka@me.com>
2015-12-06 20:58:07 -0500

    grunt changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -63,12 +63,8 @@ module.exports = function(grunt) {
                     src: [ './**/*.mp3' ],
                     dest: './dist/sounds',
                     cwd: './app/sounds'
-                },{
-                    expand: true,
-                    src: [ './CNAME' ],
-                    dest: './dist/CNAME',
-                    cwd: './CNAME'
-                }]
+                }
+              ]
             },
             cname: {
                 files: {
